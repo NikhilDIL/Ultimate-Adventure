67d9a12e003dfa5c4d48dd3113cc63bcda09a302
NikhilDIL <ndeena2@illinois.edu>
2018-11-13 22:12:09 -0600

    Added a Hero class

---
+++ UltimateAdventure/Alexander.cpp
@@ -0,0 +1,11 @@
+#include "Alexander.h"
+
+Alexander::Alexander() {}
+
+void Alexander::FirstSpecialSkill() {
+	cout << "Hello World. I'm Alexander!" << endl;
+}
+
+void Alexander::SecondSpecialSkill() {
+	cout << "Hello World. I'm Alexander!. Special Second!" << endl;
+}
---
+++ UltimateAdventure/src/Alexander.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Warrior.h"
+
+class Alexander : public Warrior {
+public:
+	Alexander(); // default constructor
+	void FirstSpecialSkill();
+	void SecondSpecialSkill();
+};
