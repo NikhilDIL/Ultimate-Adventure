ebc000f4683bcc3c995686e4e26c9391db8ab3be
NikhilDIL <ndeena2@illinois.edu>
2018-11-23 14:02:01 -0600

    Added get and set methods for row and col

---
+++ UltimateAdventure/Character.cpp
@@ -0,0 +1,22 @@
+#include "Character.h"
+
+Character::Character() {}
+
+
+int Character::getRow() {
+	return row;
+}
+
+int Character::getCol() {
+	return col;
+}
+
+void Character::setRow(int r) {
+	row = r;
+}
+
+void Character::setCol(int c) {
+	col = c;
+}
+
+
--- UltimateAdventure/src/Character.h
+++ UltimateAdventure/src/Character.h
@@ -1,7 +1,10 @@
 #pragma once
-
 class Character {
+private:
+	int row;
+	int col;
 public:
+	Character();
 	// All common among all unit classes
 	virtual void Attack() = 0;
 	virtual void Defend() = 0;
@@ -10,4 +13,10 @@ public:
 	// These vary depending on the character
 	virtual void FirstSpecialSkill() = 0;
 	virtual void SecondSpecialSkill() = 0;
+
+	void setRow(int r);
+	void setCol(int c);
+
+	int getRow();
+	int getCol();
 };
