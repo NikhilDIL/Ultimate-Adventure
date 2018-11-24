50a4de8dc6e39953e7866f42155f2d81e2cf09ea
NikhilDIL <ndeena2@illinois.edu>
2018-11-23 18:06:05 -0600

    Removed unesscessary functions from class

--- UltimateAdventure/src/Warrior.h
+++ UltimateAdventure/src/Warrior.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include "GameEngine.h"
 #include "Character.h"
 using std::cout;
 using std::endl;
@@ -19,9 +18,4 @@ public:
 	void StrongAttack();
 	virtual void FirstSpecialSkill() = 0;
 	virtual void SecondSpecialSkill() = 0;
-
-	int getLocationX();
-	int getLocationY();
-	void setLocationX(int x);
-	void setLocationY(int y);
 };
