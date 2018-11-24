13da44f7db5dea7e742163e3ae04733bb95a64da
NikhilDIL <ndeena2@illinois.edu>
2018-11-23 18:12:40 -0600

    Removed uncessary code from main

--- UltimateAdventure/src/main.cpp
+++ UltimateAdventure/src/main.cpp
@@ -2,9 +2,12 @@
 #include <vector>
 #include "ofMain.h"
 #include "ofApp.h"
-#include "GameEngine.h"
+#include "Character.h"
 #include "Warrior.h"
 #include "Alexander.h"
+
+using std::cout;
+using std::endl;
 
 //========================================================================
 int main( ){
@@ -12,20 +15,11 @@ int main( ){
 
 	std::cout << "hello world" << std::endl;
 
-	std::vector<Character*> character_vector; // all characters in game
-
-	// create character and add it to the vector
-	Alexander alexander;
-	Character* character = &alexander;
-	character_vector.push_back(character);
-
-	GameEngine gameengine(character_vector);
+	ofSetFrameRate(12);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
-
-
 
 }
