7b168b216cbdd2bbf21cde941c015d55fcf6bce5
NikhilDIL <ndeena2@illinois.edu>
2018-11-29 11:01:29 -0600

    Removed include "warrior.h" file

--- UltimateAdventure/src/main.cpp
+++ UltimateAdventure/src/main.cpp
@@ -3,7 +3,6 @@
 #include "ofMain.h"
 #include "ofApp.h"
 #include "Character.h"
-#include "Warrior.h"
 #include "Alexander.h"
 
 using std::cout;
@@ -11,15 +10,11 @@ using std::endl;
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
-	std::cout << "hello world" << std::endl;
-
-	ofSetFrameRate(12);
+	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetFrameRate(30);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
-
 }
